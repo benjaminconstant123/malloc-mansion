0ee40ea3073a1a339b92a25490c57260a3155aad
benjaminconstant123 <152168776+benjaminconstant123@users.noreply.github.com>
2025-11-25 12:50:50 +0100

    add: adding methods to Inventory.h and TODOs to Inventory.cpp

--- includes/Inventory.h
+++ includes/Inventory.h
@@ -4,14 +4,21 @@
 
 #ifndef MALLOC_MANSION_INVENTORY_H
 #define MALLOC_MANSION_INVENTORY_H
+#include <vector>
 
+#include "Item/Item.h"
 
 class Inventory {
     protected:
+    std::vector<Item *> items;
     int size;
     public:
     Inventory(int size);
     void set_size(int size);
+    void set_items(std::vector<Item *> items); //pour set plein d'item
+    std::vector<Item *> get_items() const; //pour get plein d'item
+    void add_item(Item *item); //pour add un seul item
+    void remove_item(Item *item); //pour remove un item
 
 };
 
--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -12,3 +12,19 @@ void Inventory::set_size(int size) {
     this->size += size;
 }
 
+void Inventory::set_items(std::vector<Item *> items) {
+    //TODO : implementation a faire
+}
+
+std::vector<Item *> Inventory::get_items() const {
+    //TODO : implementation a faire
+}
+
+void Inventory::add_item(Item *item) {
+    //TODO : implementation a faire
+}
+
+void Inventory::remove_item(Item *item) {
+    //TODO : implementation a faire
+}
+
