ec7ca2cc6324afddd28d1388a12a407cf8b81265
benjaminconstant123 <benjamin.constant821@gmail.com>
2025-11-25 16:07:46 +0100

    add : c'est cool Valgrind mdr

--- main.cpp
+++ main.cpp
@@ -1,25 +1,28 @@
-//
-// Created by benja on 22/11/2025.
-//
+    //
+    // Created by benja on 22/11/2025.
+    //
 
-#include <iostream>
-#include <ostream>
+    #include <iostream>
+    #include <ostream>
 
-#include "includes/Enemy.h"
-#include "includes/Player.h"
-#include "includes/Item/HealingPotion.h"
+    #include "includes/Enemy.h"
+    #include "includes/Player.h"
+    #include "includes/Item/HealingPotion.h"
 
-int main() {
-    Player *player = new Player("john", 100, 10, 1);
-    Enemy *enemy = new Enemy("jerome le monstre", 20,15,1);
+    int main() {
+        Player *player = new Player("john", 100, 10, 1);
+        Enemy *enemy = new Enemy("jerome le monstre", 20,15,1);
 
-    player->take_damage(50);
-    HealingPotion *potion = new HealingPotion(5);
-    potion->use(player);
+        player->take_damage(50);
+        HealingPotion *potion = new HealingPotion(5);
+        potion->use(player);
 
-    std::string name = player->get_name();
-    std::cout << name << std::endl;
+        delete potion;
 
+        std::string name = player->get_name();
+        std::cout << name << std::endl;
 
-    return 0;
-}
+        delete player;
+        delete enemy;
+        return 0;
+    }
