1acf0b9e8380bbd944115b82754ce42b3201c0b9
Xelax-XIII <gabax.barsav@gmail.com>
2025-11-28 14:57:16 +0100

    update : removed the useless comments

--- src/Dice.cpp
+++ src/Dice.cpp
@@ -18,19 +18,8 @@ int Dice::get_face() {
 }
 
 int Dice::random_face() {
-    // 1. Initialisation du "grain" (seed)
-    // On utilise random_device pour obtenir une graine depuis le matériel
     std::random_device rd;
-
-    // 2. Le Moteur (Le générateur de nombres pseudo-aléatoires)
-    // 'static' est CRUCIAL ici : on ne veut initialiser le générateur qu'une seule fois
-    // pour éviter de générer la même suite de nombres si la fonction est appelée très vite.
     static std::mt19937 gen(rd());
-
-    // 3. La Distribution
-    // Définit la plage [1, n] (inclusif)
     std::uniform_int_distribution<> distrib(1, this->face);
-
-    // 4. Génération
     return distrib(gen);
 }
